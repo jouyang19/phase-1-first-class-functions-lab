a0c5712a2f817365a3b6a26a08a3e0cd68b0dce5
Guang <jouyang19@proton.me>
2024-04-29 13:47:26 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(randomArray) {
+    const newArr = randomArray.slice(0,2);
+    return newArr;
+}
+
+function returnLastTwoDrivers(randomArray) {
+    const newArr = randomArray.slice(-2);
+    return newArr;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(randomInt) {
+    return function(fare) {
+        return fare * randomInt;
+    }
+}
+
+const doubleMultiplier = createFareMultiplier(2);
+
+function fareDoubler(fare) {
+    return doubleMultiplier(fare);
+}
+
+const tripleMultiplier = createFareMultiplier(3);
+
+function fareTripler(fare) {
+    return tripleMultiplier(fare);
+}
+
+function selectDifferentDrivers(randomArr, callbackFunction) {
+    const newArr = callbackFunction(randomArr);
+    return newArr;
+}
